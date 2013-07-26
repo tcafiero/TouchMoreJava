c115fbcb000e4c99f8e218aed13e1eb1aabd83e1
Toni Cafiero <toni@cafiero.it>
2013-07-27 00:04:30 +0200

    some fixies

--- src/Try.java
+++ src/Try.java
@@ -1,25 +1,23 @@
 //  File : ./main.java
 
-public class Try
-{
-  // Operations
-  
-  public static void main(String[] argv)
-  {
-  // ## OperationBody [6839a6cb-5dc2-4d62-9c98-d500c7485fdf] 
-  		     System.loadLibrary("dsp");
-    
-  // Roles
-  
-  
-  
-  // 	dsp dsp = new dsp();
-  	GUI_SIMULATION gui = new GUI_SIMULATION();
-  	gui.Click();
-  gui.ClickDx();
-  gui.Click();
-  gui.Click();
-  // ## OperationBody End 
-  }
+public class Try {
+	// Operations
+
+	public static void main(String[] argv) {
+		// ## OperationBody [6839a6cb-5dc2-4d62-9c98-d500c7485fdf]
+//		System.loadLibrary("dirana");
+		System.loadLibrary("dsp");
+
+		// Roles
+
+		// dsp dsp = new dsp();
+		GUI_SIMULATION gui = new GUI_SIMULATION();
+		gui.Click();
+		gui.ClickDx();
+		gui.Click();
+		gui.ClickDx();
+		gui.Click();
+		// ## OperationBody End
+	}
 
 }
