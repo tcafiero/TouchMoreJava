363e67c7d265d40888abb52a25075bb2d714d481
Toni Cafiero <toni@cafiero.it>
2013-07-27 17:42:29 +0200

    update

--- src/Try.java
+++ src/Try.java
@@ -5,12 +5,10 @@ public class Try {
 
 	public static void main(String[] argv) {
 		// ## OperationBody [6839a6cb-5dc2-4d62-9c98-d500c7485fdf]
-//		System.loadLibrary("dirana");
 		System.loadLibrary("dsp");
 
 		// Roles
 
-		// dsp dsp = new dsp();
 		GUI_SIMULATION gui = new GUI_SIMULATION();
 		gui.Click();
 		gui.ClickDx();
